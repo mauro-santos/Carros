cbdaae69eae0534a9e2cc07397d7aaaa221534ba
Mauro Antonio dos Santos <santos.mauroantonio@gmail.com>
2020-07-25 11:13:30 -0300

    - Parser json (construtores): getRoles com for (each).

--- lib/pages/usuario.dart
+++ lib/pages/usuario.dart
@@ -10,10 +10,20 @@ class Usuario {
       : this.login = map["login"],
         this.nome = map["nome"],
         this.email = map["email"],
-        this.token = map["token"];
+        this.token = map["token"],
+        this.roles = getRoles(map);
 
   @override
   String toString() {
     return 'Usuario{login: $login, nome: $nome, email: $email, token: $token, roles: $roles}';
+  }
+
+  static List<String> getRoles(Map<String, dynamic> map) {
+    List list = map["roles"];
+    List<String> roles = [];
+    for (String role in list) {
+      roles.add(role);
+    }
+    return roles;
   }
 }
