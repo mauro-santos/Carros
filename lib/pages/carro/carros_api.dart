a5bb693d4b9967cdf1eca3f7ff447a6e9069c685
Mauro Antonio dos Santos <santos.mauroantonio@gmail.com>
2020-09-04 01:52:50 -0300

    - Classe Carro e Listview de carro com ListTile, depois com Row (com Text em Flexible e overflow ellipsis)

---
+++ lib/pages/carro/carros_api.dart
@@ -0,0 +1,25 @@
+import 'package:carros/pages/carro/carro.dart';
+
+class CarrosApi {
+  static List<Carro> getCarros() {
+    final carros = List<Carro>();
+
+    carros.add(Carro(
+      nome: "AUDI GT Spyder",
+      urlFoto:
+          "https://s3-sa-east-1.amazonaws.com/videos.livetouchdev.com.br/esportivos/Audi_Spyder.png",
+    ));
+    carros.add(Carro(
+      nome: "Porsche Panamera",
+      urlFoto:
+          "https://s3-sa-east-1.amazonaws.com/videos.livetouchdev.com.br/esportivos/Porsche_Panamera.png",
+    ));
+    carros.add(Carro(
+      nome: "Lamborghini Aventador",
+      urlFoto:
+          "https://s3-sa-east-1.amazonaws.com/videos.livetouchdev.com.br/esportivos/Lamborghini_Aventador.png",
+    ));
+
+    return carros;
+  }
+}
