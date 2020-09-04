c5faa988a8dd1f787b1ac94eaaed1b63af140e71
Mauro Antonio dos Santos <santos.mauroantonio@gmail.com>
2020-09-04 17:49:33 -0300

    - Layout com Card e ButtonTheme.bar deprecated, utilizando ButtonBar diretamente (ButtonBarTheme).

--- lib/pages/carro/home_page.dart
+++ lib/pages/carro/home_page.dart
@@ -18,37 +18,59 @@ class HomePage extends StatelessWidget {
   _body() {
     List<Carro> carros = CarrosApi.getCarros();
 
-    return ListView.builder(
-      itemCount: carros.length,
-      itemBuilder: (context, index) {
-        Carro c = carros[index];
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: ListView.builder(
+        itemCount: carros.length,
+        itemBuilder: (context, index) {
+          Carro c = carros[index];
 
-        return Row(
-          children: [
-            Image.network(
-              c.urlFoto,
-              width: 150,
-            ),
-            Flexible(
-              child: Text(
-                c.nome,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(fontSize: 25),
+          return Card(
+            color: Colors.grey[100],
+            elevation: 2.0,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(
+                    child: Image.network(
+                      c.urlFoto,
+                      width: 250,
+                    ),
+                  ),
+                  Text(
+                    c.nome,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  Text(
+                    "descrição...",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  /*ButtonBarTheme(
+                    data: ButtonBarTheme.of(context),
+                    child:*/
+                  ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: const Text('DETALHES'),
+                        onPressed: () {/* ... */},
+                      ),
+                      FlatButton(
+                        child: const Text('SHARE'),
+                        onPressed: () {/* ... */},
+                      ),
+                    ],
+                  ),
+                  //),
+                ],
               ),
             ),
-          ],
-        );
-        /*
-        return ListTile(
-          leading: Image.network(c.urlFoto),
-          title: Text(
-            c.nome,
-            style: TextStyle(fontSize: 22),
-          ),
-        );
-        */
-      },
+          );
+        },
+      ),
     );
   }
 }
