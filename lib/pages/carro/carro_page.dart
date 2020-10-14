8fc587fdb28c939abf8b5d45c62cc124d7896485
Mauro Antonio dos Santos <santos.mauroantonio@gmail.com>
2020-10-14 09:02:30 -0300

    - Carro Detalhes: Layout parte 1 com row e column, divider, parte 2 com scroll (LoremIpsum). - método/widget text.

--- lib/pages/carro/carro_page.dart
+++ lib/pages/carro/carro_page.dart
@@ -1,4 +1,5 @@
 import 'package:carros/pages/carro/carro.dart';
+import 'package:carros/widgets/text.dart';
 import 'package:flutter/material.dart';
 
 class CarroPage extends StatelessWidget {
@@ -42,7 +43,63 @@ class CarroPage extends StatelessWidget {
   _body() {
     return Container(
       padding: EdgeInsets.all(16),
-      child: Image.network(carro.urlFoto),
+      child: ListView(
+        children: <Widget>[
+          Image.network(carro.urlFoto),
+          _bloco1(),
+          Divider(),
+          _bloco2(),
+        ],
+      ),
+    );
+  }
+
+  Row _bloco1() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            text(carro.nome, fontSize: 20, bold: true),
+            text(carro.tipo, fontSize: 16),
+          ],
+        ),
+        //Spacer(),
+        Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.red,
+                size: 40,
+              ),
+              onPressed: _onClickFavorito,
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.share,
+                size: 40,
+              ),
+              onPressed: _onClickShare,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  _bloco2() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 20),
+        text(carro.descricao, fontSize: 16, bold: true),
+        SizedBox(height: 20),
+        text(
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            fontSize: 16),
+      ],
     );
   }
 
@@ -63,4 +120,8 @@ class CarroPage extends StatelessWidget {
         break;
     }
   }
+
+  _onClickFavorito() {}
+
+  _onClickShare() {}
 }
