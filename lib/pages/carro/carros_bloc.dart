fc2e047ad48682db35aa7e3cbea4785b13e01c6b
Mauro Antonio dos Santos <santos.mauroantonio@gmail.com>
2020-10-20 16:20:53 -0300

    - Refresh Indicator (pequenas alterações).

--- lib/pages/carro/carros_bloc.dart
+++ lib/pages/carro/carros_bloc.dart
@@ -4,16 +4,16 @@ import 'package:carros/pages/carro/simple_bloc.dart';
 
 class CarrosBloc extends SimpleBloc<List<Carro>> {
   Future<List<Carro>> fetch(String tipo) async {
-    List<Carro> carros;
-
     try {
-      carros = await CarrosApi.getCarros(tipo);
+      List<Carro> carros = await CarrosApi.getCarros(tipo);
 
       add(carros);
+
+      return carros;
     } on Exception catch (e) {
       addError(e);
     }
 
-    return carros;
+    return null;
   }
 }
