ec6efc5393326b139306ef863bab5125e1381a4c
Mauro Antonio dos Santos <santos.mauroantonio@gmail.com>
2021-01-03 11:42:05 -0300

    - Cache com banco de dados e package Connectivity buscando online se conectado ou local se offline.

--- lib/pages/carros/carros_bloc.dart
+++ lib/pages/carros/carros_bloc.dart
@@ -1,11 +1,23 @@
 import 'package:carros/pages/carros/carro.dart';
 import 'package:carros/pages/carros/carros_api.dart';
 import 'package:carros/pages/carros/simple_bloc.dart';
+import 'package:carros/pages/favoritos/carro_dao.dart';
+import 'package:carros/utils/network.dart';
 
 class CarrosBloc extends SimpleBloc<List<Carro>> {
   Future<List<Carro>> fetch(String tipo) async {
+    List<Carro> carros;
+
     try {
-      List<Carro> carros = await CarrosApi.getCarros(tipo);
+      bool networkOn = await isNetworkOn();
+
+      print("NetworkOn? $networkOn");
+
+      if (!networkOn) {
+        carros = await CarroDAO().findAllByTipo(tipo);
+      } else {
+        carros = await CarrosApi.getCarros(tipo);
+      }
 
       add(carros);
 
